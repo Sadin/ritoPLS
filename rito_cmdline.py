74ab4e0e8c483e3df9b76849ae94aaaf1fa17457
Zachary Snyder <sadin@fedoraproject.org>
2015-02-24 11:50:15 -0500

    Made some stuff look nicer

--- rito_cmdline.py
+++ rito_cmdline.py
@@ -1,17 +1,24 @@
 import requests, json, pprint
 
 cycle = True
+first_time = True
 
 while cycle == True:
     # Ask user from a list of functions what they would like to do.
-    user_intention = raw_input("Welcome to summoner spyglass, what would you like to do?")
+    if first_time == True:
+        user_intention = raw_input("Welcome to summoner spyglass, how can I help? ( Type 'help' for a list of options ) ")
+    elif first_time == False
 
+    # Stop the loop and exit,
+    if user_intention == "exit":
+        print("Bye, Bye!")
+        break
     # If the user would like to find details of a Summoner prompt them for the name and verify said name exists.
-    if user_intention == "Summoner":
+    elif user_intention == "Summoner":
         summoner_name = raw_input("Enter summoner name: ")
 
     # Verify the summoner_name input was correct, if not break the loop
-        verify = raw_input("Im going to grab details of {}, is that correct? (yes/no)".format(summoner_name))
+        verify = raw_input("Im going to grab details of {}, is that correct? (yes/no) ".format(summoner_name))
 
         if verify == "yes":
             print("Checking details on {}".format(summoner_name))
@@ -22,11 +29,19 @@ while cycle == True:
 
             if r.status_code == 200:
                 print("Success! The summoner {} was found!".format(summoner_name))
+
+                pprint.pprint(r.json())
             else:
                 print("Sorry, {} wasnt found. Please try again!".format(summoner_name))
-        else verify == "no":
-            break
+
+        elif verify == "no":
+            print("Okay, Lets try that again!")
 
 
-        pprint.pprint(r.json())
+    # If the user input stored at user_intention makes no sense, restart the loop with error message and reprompt.
+
+    else:
+        first_time = False
+
+ 
 
