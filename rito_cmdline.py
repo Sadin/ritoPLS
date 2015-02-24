01d91098b2f4a99286db09fd33fef3617570be9c
Zachary Snyder <sadin@fedoraproject.org>
2015-02-24 12:38:59 -0500

    added help menu

--- rito_cmdline.py
+++ rito_cmdline.py
@@ -1,6 +1,6 @@
 import requests, json, pprint
 
-# Declare some stuff here, Best stuff Declared NA
+# Declare some stuff here, Best stuff Declared NA 
 cycle = True
 error = False
 error_message = "Something didnt work cause Sadin is a baddie."
@@ -13,9 +13,9 @@ while cycle == True:
         user_intention = raw_input("Welcome to ritoCMD, how can I help: ( Type 'help' for a list of options ) ")
 
     elif error == True:
-        user_intention = raw_input("You're request was not recognized, make sure you've entered it correctly, and try again: ( Type 'help' for a list of options ) ")
-    else
-        return error_message
+        user_intention = raw_input("You're request was not recognized, make sure you've entered it correctly \nand try again: ( Type 'help' for a list of options ) ")
+    else:
+        print error_message
     # Stop the loop and exit,
     if user_intention == "exit":
         print("Bye, Bye!")
@@ -28,14 +28,14 @@ while cycle == True:
         verify = raw_input("Im going to grab details of {}, is that correct? (yes/no) ".format(summoner_name))
 
         if verify == "yes":
-            print("Checking details on {}".format(summoner_name))
+            print("Checking details on {}\n".format(summoner_name))
 
             payload = {'api_key': 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'}
 
             r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(summoner_name), params=payload)
 
             if r.status_code == 200:
-                print("Success! The summoner {} was found!".format(summoner_name))
+                print("Success! The summoner {} was found!\n".format(summoner_name))
 
                 pprint.pprint(r.json())
             else:
@@ -43,12 +43,13 @@ while cycle == True:
 
         elif verify == "no":
             print("Okay, Lets try that again!")
-
+    elif user_intention == "Help" or user_intention == "help":
+        print "\nHere is a list of valid inputs:\n      Summoner - Prints info about a summoner \n      exit - Exits the program \n"
 
     # If the user input stored at user_intention makes no sense, restart the loop with error message and reprompt.
 
     else:
-        error = False
+        error = True
 
 
 
