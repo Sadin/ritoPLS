56fc5b5385ed7eb9ef3d5a35b4041049d926ea84
Zachary Snyder <sadin@fedoraproject.org>
2015-02-24 13:02:34 -0500

    wrapped "summoner" function in a while loop

--- rito_cmdline.py
+++ rito_cmdline.py
@@ -5,6 +5,10 @@ cycle = True
 error = False
 error_message = "Something didnt work cause Sadin is a baddie."
 
+# Declarations for Summoner Details while loop
+summoner_error = False
+
+
 while cycle == True:
     
     # Check if the loop was restarted due to an error.
@@ -22,27 +26,37 @@ while cycle == True:
         break
     # If the user would like to find details of a Summoner prompt them for the name and verify said name exists.
     elif user_intention == "Summoner":
-        summoner_name = raw_input("Enter summoner name: ")
 
-    # Verify the summoner_name input was correct, if not break the loop
-        verify = raw_input("Im going to grab details of {}, is that correct? (yes/no) ".format(summoner_name))
+        # Begin the loop for the Summoner Infomation Function
+        while summoner_error == False:
+            summoner_name = raw_input("Enter summoner name: ")
 
-        if verify == "yes":
-            print("Checking details on {}\n".format(summoner_name))
+        # Verify the summoner_name input was correct, if not break the loop
+            verify = raw_input("Im going to grab details of {}, is that correct? (yes/no) ".format(summoner_name))
 
-            payload = {'api_key': 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'}
+            if verify == "yes":
+                print("Checking details on {}\n".format(summoner_name))
 
-            r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(summoner_name), params=payload)
+                payload = {'api_key': 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'} 
 
-            if r.status_code == 200:
-                print("Success! The summoner {} was found!\n".format(summoner_name))
+                r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(summoner_name), params=payload) #format the API request URL
 
-                pprint.pprint(r.json())
-            else:
-                print("Sorry, {} wasnt found. Please try again!".format(summoner_name))
+                if r.status_code == 200:
+                    print("Success! The summoner {} was found!\n".format(summoner_name))
 
-        elif verify == "no":
-            print("Okay, Lets try that again!")
+                    pprint.pprint(r.json())
+
+                    # Check if the user would like to check the details of another summoner,
+                    # If not Send them back to the function selection prompt.
+
+                    if raw_input("\nWould you like to check on another summoner? ( yes/no ) ") == "no":
+                        break  # exit this loop
+
+                else:
+                    print("Sorry, {} wasnt found. Please try again!".format(summoner_name))
+
+            elif verify == "no":
+                print("Okay, Lets try that again!")
     elif user_intention == "Help" or user_intention == "help":
         print "\nHere is a list of valid inputs:\n      Summoner - Prints info about a summoner \n      exit - Exits the program \n"
 
