481c30f1262168c0f5660424927dce037eaf5b72
Zachary Snyder <sadin@fedoraproject.org>
2015-02-24 11:19:13 -0500

    adding some comments

--- rito_cmdline.py
+++ rito_cmdline.py
@@ -1,25 +1,29 @@
 import requests, json, pprint
 
-user_intention = raw_input("Welcome to summoner spyglass, what would you like to do?")
-if user_intention == "Summoner":
-    summoner_name = raw_input("Enter summoner name: ")
+while True:
+    # Ask user from a list of functions what they would like to do.
+    user_intention = raw_input("Welcome to summoner spyglass, what would you like to do?")
 
-    verify = raw_input("Im going to grab details of {}, is that correct? (yes/no)".format(summoner_name))
+    # If the user would like to find details of a Summoner prompt them for the name and verify said name exists.
+    if user_intention == "Summoner":
+        summoner_name = raw_input("Enter summoner name: ")
 
-    if verify == "yes":
-        print("Checking details on {}".format(summoner_name))
+        verify = raw_input("Im going to grab details of {}, is that correct? (yes/no)".format(summoner_name))
 
-        payload = {'api_key': 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'}
+        if verify == "yes":
+            print("Checking details on {}".format(summoner_name))
 
-        r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(summoner_name), params=payload)
+            payload = {'api_key': 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'}
 
-        if r.status_code == 200:
-            print("Success! The summoner {} was found!".format(summoner_name))
-        else:
-            print("Sorry, {} wasnt found. Please try again!".format(summoner_name))
-    else verify == "no":
-        break
+            r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(summoner_name), params=payload)
+
+            if r.status_code == 200:
+                print("Success! The summoner {} was found!".format(summoner_name))
+            else:
+                print("Sorry, {} wasnt found. Please try again!".format(summoner_name))
+        else verify == "no":
+            break
 
 
-    pprint.pprint(r.json())
+        pprint.pprint(r.json())
 
