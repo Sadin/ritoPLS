19ccc29c98b2e2e42d66a13bbc2b3ea109c21e6b
Dylan Leh <ruinsane@fedoraproject.org>
2015-03-05 20:15:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json, requests
 
 payload = {'api_key': 'e3ab974f-7a2f-417c-b31d-78c86c1dd190'}
 
-r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Ruinsane", params  = payload)
+r = requests.get("http://status.leagueoflegends.com/shards", params  = payload)
 
 if r.status_code == 200:
 	print("Success!")
