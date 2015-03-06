d952e89a998342f4dd7067d46ff3d7edd7298f63
Zachary Snyder <sadin@fedoraproject.org>
2015-03-06 12:42:16 -0500

    working on riot.py

--- riot.py
+++ riot.py
@@ -1,16 +1,15 @@
 import requests, json
 
 class summoner():
-    def __init__(self, name):
+    def __init__(self, **kwargs):
 
         api_key = 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'
         payload = {'api_key': api_key}
         result = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(name), params=payload) #format the API request URL
-
-        self.name = name
+        self.name = kwargs.get('name', 'name')
         self.status = result.status_code
 
     def basic_info(self):
-    	return self.name.upper()
+    	return result.json()
 
 		
