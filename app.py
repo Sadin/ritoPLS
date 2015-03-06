5946ee21c870c80aafeb1136f839ffc6caf7308c
Zachary Snyder <sadin@fedoraproject.org>
2015-03-06 12:01:07 -0500

    spaces

--- app.py
+++ app.py
@@ -9,26 +9,26 @@ app = Flask(__name__, static_path='/static')
 
 @app.route('/')
 def index():
-	return render_template('index.html', landing=True)
+    return render_template('index.html', landing=True)
 
 @app.route('/summoner', methods=['GET', 'POST'])
 def summoner():
-	error = False
-	bad_name = False
-	if request.method == 'POST':
-		user_name = request.form['name']
-		payload = {'api_key': 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'}
+    error = False
+    bad_name = False
+    if request.method == 'POST':
+        user_name = request.form['name']
+        payload = {'api_key': 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'}
 
-		# attempt to query the riot games api for the summoner basic info
-		s_info = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(user_name), params=payload) #format the API request URL
-		if s_info.status_code == 200:
-			print("SUCCESS! The summoner {} was found!".format(user_name))
-			error = False
-		else:
-			print("ERROR The summoner {} was not found!".format(user_name))
-			error = True
+        # attempt to query the riot games api for the summoner basic info
+        s_info = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(user_name), params=payload) #format the API request URL
+        if s_info.status_code == 200:
+            print("SUCCESS! The summoner {} was found!".format(user_name))
+            error = False
+        else:
+            print("ERROR The summoner {} was not found!".format(user_name))
+            error = True
 
-		return render_template('summoner.html', summoner=request.form['name'], failure=error)
+        return render_template('summoner.html', summoner=request.form['name'], failure=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- riot.py
+++ riot.py
@@ -1,19 +1,16 @@
 import requests, pprint, json
 
-
-
 class summoner():
+    def __init__(self, name):
 
-	def __init__(self, name):
+        api_key = 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'
+        payload = {'api_key': api_key}
+        result = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(name), params=payload) #format the API request URL
 
-		api_key = 'b0afea27-1602-4ed8-aff5-24caf6bbb2d1'
-		payload = {'api_key': api_key}
-		result = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/{}".format(name), params=payload) #format the API request URL
+        self.name = name
+        self.status = result.status_code
 
-		self.name = name
-		self.status = result.status_code
-
-	def basic_info(self, name):
-		return 0
+    def basic_info(self, name):
+		
 
 		
