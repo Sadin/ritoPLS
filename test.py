dc5176ae9eb4c33ccd80c15c0eb399e2abe5a5aa
Dylan Leh <Ruinsane@localhost.localdomain>
2015-03-05 20:11:25 -0500

    Heres the intro to API. Woooooooooooo

---
+++ test.py
@@ -0,0 +1,12 @@
+import json, requests
+
+payload = {'api_key': 'e3ab974f-7a2f-417c-b31d-78c86c1dd190'}
+
+r = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Ruinsane", params  = payload)
+
+if r.status_code == 200:
+	print("Success!")
+else:
+	print("Ya done fucked up, Bob")
+
+
